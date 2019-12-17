5ef40cc714e8eb91e448f55665fce4fd908f7207
Kozasa Takuro <kozasatakuro@Kozasa-no-Air.lan>
2019-12-17 15:01:10 +0900

    Create main screen

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import MemoList from './src/components/MemoList';
+import Appbar from './src/components/Appbar';
+import CircleButton from './src/elements/CircleButton';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar />
+        <MemoList />
+        <CircleButton />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
   },
 });
